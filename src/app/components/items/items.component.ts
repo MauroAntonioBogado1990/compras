26c2b19d98e9b7e21bb0bdfa1526e8cb00216203
Mauro Bogado <mauroantoniobogado@gmail.com>
2021-03-25 19:10:26 -0300

    primera carga de lista de compras

---
+++ src/app/components/items/items.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Item } from '../../models/item';
+@Component({
+  selector: 'app-items',
+  templateUrl: './items.component.html',
+  styleUrls: ['./items.component.css']
+})
+export class ItemsComponent implements OnInit {
+
+  items: Item[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        id: 0,
+        title: 'remera',
+        price: 700,
+        quantity: 2,
+        completed: false
+      },
+      {
+        id: 1,
+        title: 'pantalon',
+        price: 500,
+        quantity: 1,
+        completed: true
+      }
+    ]
+  }
+
+}
